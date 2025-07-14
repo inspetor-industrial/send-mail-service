29deffb911f418f926cc28be92028c47de87242c
pedroaba <pedr.augustobarbosa.aparecido@gmail.com>
2025-07-13 21:20:40 -0300

    Update server port configuration to use environment variable or default to 3000

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ const app = new Elysia()
   )
   .use(healthy)
   .use(sendMail)
-  .listen(3000)
+  .listen(process.env.PORT || 3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
